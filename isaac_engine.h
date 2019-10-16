022f057a625ecdcd6b4e27f1513a36c59b1cab63
daggilli <daggillies@gmail.com>
2019-10-16 16:44:23 -0600

    initial commit

---
+++ isaac_engine.h
@@ -0,0 +1,37 @@
+#ifndef __ISAAC_ENGINE_H__
+#define __ISAAC_ENGINE_H__
+
+#include <algorithm>
+#include <cstdint>
+#include <random>
+#include "isaac.h"
+
+namespace IsaacEngine {
+  class IsaacEngine {
+   public:
+    using result_type = uint32_t;
+    static constexpr result_type(min)() { return 0; }
+    static constexpr result_type(max)() { return UINT32_MAX; }
+    friend bool operator==(const IsaacEngine &lhs, const IsaacEngine &rhs);
+    friend bool operator!=(const IsaacEngine &lhs, const IsaacEngine &rhs);
+
+    IsaacEngine() : prng() {}
+    explicit IsaacEngine(std::random_device &rd) : prng(rd) {}
+
+    void seed(std::random_device &rd) { prng.seed(rd); }
+
+    result_type operator()() { return prng.rand(); }
+
+    void discard(unsigned long long n) {
+      for (unsigned long long i = 0; i < n; i++) operator()();
+    }
+
+   private:
+    Isaac::Isaac prng;
+  };
+
+  bool operator==(const IsaacEngine &lhs, const IsaacEngine &rhs) { return lhs.prng == rhs.prng; }
+
+  bool operator!=(const IsaacEngine &lhs, const IsaacEngine &rhs) { return !(lhs.prng == rhs.prng); }
+}  // namespace IsaacEngine
+#endif
