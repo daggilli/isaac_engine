74b158567923c895cad209658a0a909159a69f1e
David Gillies <david.gillies@sbrforum.com>
2019-10-18 15:45:42 -0600

    test: add test vectors

--- test/unittest/isaac_engine.test.cpp
+++ test/unittest/isaac_engine.test.cpp
@@ -7,11 +7,9 @@
 
 // g++ -std=c++11 -Wall -o isaac_enging.test isaac_engine.test.cpp
 
-std::random_device rd;
-
-IsaacEngine::IsaacEngine engine;
+// std::random_device rd;
 
 TEST_CASE("Single random value from engine with zero key (pass)", "[single-file]") {
-  //
+  IsaacEngine::IsaacEngine engine;
   REQUIRE(engine() == 0x182600f3);
 }
